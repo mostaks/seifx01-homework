0533c30b088fff502bc33444df105cf4a2ebd8bc
Gabriel Conceicao <gabrielcon6cao@gmail.com>
2019-08-20 13:24:07 +1000

    including bg and mario lives

--- gabriel_conceicao/w00d3/marioDemo1/script.js
+++ gabriel_conceicao/w00d3/marioDemo1/script.js
@@ -1,6 +1,25 @@
 var canvas = document.getElementById('canvas');
 var graphics = canvas.getContext('2d');
 
+var bgTexture = new Image();
+bgTexture.src = "https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/0kjHIH6/old-retro-video-game-arcade-clouds-moving-on-a-blue-sky_hcd0pxim__F0000.png";
+
+var bgWidth = canvas.width;
+var bgHeight = canvas.height;
+var bgX = 0;
+var bgY = 0;
+
+var bg2Texture = new Image();
+bg2Texture.src = "https://previews.123rf.com/images/choostudio/choostudio1803/choostudio180300049/97413372-vector-game-over-phrase-in-pixel-art-8-bit-style-with-glitch-vhs-effect-three-color-half-shifted-let.jpg";
+
+var bg2Width = canvas.width;
+var bg2Height = canvas.height;
+var bg2X = 0;
+var bg2Y = 0;
+
+var marioTexture = new Image();
+marioTexture.src = "http://vignette3.wikia.nocookie.net/fantendo/images/5/58/8bitsprite-1-.png/revision/latest?cb=20151029181053";
+
 var marioWidth = 64;
 var marioHeight = 64;
 var marioPositionX = canvas.width / 2;
@@ -11,68 +30,77 @@ var jumpForce = 20;
 var maxJumpForce = 20;
 var jumpForceDecay = 1;
 
-var marioTexture = new Image();
-marioTexture.src = "http://vignette3.wikia.nocookie.net/fantendo/images/5/58/8bitsprite-1-.png/revision/latest?cb=20151029181053";
 
 var goombaTexture = new Image();
-goombaTexture.src = 
-"https://static.giantbomb.com/uploads/original/9/93854/2438851-goomba%20smb%20sprite%20walk%20gif.gif";
+goombaTexture.src = "https://static.giantbomb.com/uploads/original/9/93854/2438851-goomba%20smb%20sprite%20walk%20gif.gif";
+
 
 var goombaWidth = 64;
 var goombaHeight = 64;
 var goombaPositionX = 10;
-var goombaPositionY = canvas.height-goombaHeight;
-var goombaMoveSpeed = 3;
+var goombaPositionY = canvas.height - goombaHeight;
+var goombaSpeed = 3;
 
- 
+var miniMarioWidth = 32;
+var miniMarioHeight = 32;
+var miniMarioX = miniMarioWidth;
+var miniMarioY = miniMarioHeight;
+var miniMario = marioTexture;
+
+var miniMario2Width = miniMarioWidth;
+var miniMario2Height = miniMarioHeight;
+var miniMario2X = miniMarioWidth*2;
+var miniMario2Y = miniMarioHeight;
+var miniMario2 = marioTexture;
+
+var miniMario3Width = miniMarioWidth;
+var miniMario3Height = miniMarioHeight;
+var miniMario3X = miniMarioWidth*3;
+var miniMario3Y = miniMarioHeight;
+var miniMario3 = marioTexture;
+
+var miniMario4Width = miniMarioWidth;
+var miniMario4Height = miniMarioHeight;
+var miniMario4X = miniMarioWidth*4;
+var miniMario4Y = miniMarioHeight;
+var miniMario4 = marioTexture;
+
+var miniMario5Width = miniMarioWidth;
+var miniMario5Height = miniMarioHeight;
+var miniMario5X = miniMarioWidth*5;
+var miniMario5Y = miniMarioHeight;
+var miniMario5 = marioTexture;
+
+var marioLives = 5;
+
+
+
 function update() {
-
-	// BEGIN UPDATE LOGIC
-	//-------------------------------------
+	
 	if (isKeyDown(LEFT_KEY)) {
-		marioPositionX -= marioMoveSpeed;
+		marioPositionX = marioPositionX - marioMoveSpeed;
 	}
 
-  	if (isKeyDown(RIGHT_KEY)) {
-		marioPositionX += marioMoveSpeed;
-
+	if (isKeyDown(RIGHT_KEY)) {
+		marioPositionX = marioPositionX + marioMoveSpeed;
 	}
 
-  if (isKeyDown(UP_KEY)) {
-    marioPositionY -= jumpForce;
-    jumpForce -= jumpForceDecay;
-  }
+	if(isKeyDown(UP_KEY)) {
+		marioPositionY -= jumpForce;
+		jumpForce -= jumpForceDecay;
+	}
 
-  marioPositionY += gravity;
+	if( marioPositionX < 0) {
+		marioPositionX = canvas.width;
+	}
 
-  if( marioPositionY > canvas.height - marioHeight) {
-      marioPositionY = canvas.height - marioHeight;
-      jumpForce = maxJumpForce;
-  }
-
-
-  if( marioPositionX > canvas.width) {
-      marioPositionX = 0;
-  }
-
-  if( marioPositionX < 0) {
-      marioPositionX = canvas.width;
-  }
-  if(marioPositionX > goombaPositionX + goombaWidth){
-    goombaPositionX += goombaMoveSpeed;
-  }
-
-  if(marioPositionX + marioWidth < goombaPositionX){
-    goombaPositionX -= goombaMoveSpeed;
-  }
-	//-------------------------------------
-	// END UPDATE LOGIC
-
-	// BEGIN DRAW LOGIC
-	//-------------------------------------
-
-	graphics.clearRect(0, 0, canvas.width, canvas.height)
-
+  if (marioLives === 5) {
+    	graphics.clearRect(0, 0, canvas.width, canvas.height)
+      
+    graphics.drawImage(
+		bgTexture,
+		bgX, bgY, bgWidth, bgHeight);
+    
 	graphics.drawImage(
 		marioTexture,
 		marioPositionX, marioPositionY, marioWidth, marioHeight);
@@ -81,15 +109,188 @@ function update() {
 		goombaTexture,
 		goombaPositionX, goombaPositionY, goombaWidth, goombaHeight);
 
-	//-------------------------------------
+  graphics.drawImage(
+	  miniMario,
+	  miniMarioX, miniMarioY, miniMarioWidth, miniMarioHeight);
 
-	// ask the browser to call the update function again.
-	requestAnimationFrame(update)
+  graphics.drawImage(
+	  miniMario2,
+	  miniMario2X, miniMario2Y, miniMario2Width, miniMario2Height);
+
+  graphics.drawImage(
+	  miniMario3,
+	  miniMario3X, miniMario3Y, miniMario3Width, miniMario3Height);
+
+  graphics.drawImage(
+	  miniMario4,
+	  miniMario4X, miniMario4Y, miniMario4Width, miniMario4Height);
+
+  graphics.drawImage(
+	  miniMario5,
+	  miniMario5X, miniMario5Y, miniMario5Width, miniMario5Height);
+  }
+
+  if (marioLives === 4) {
+    	graphics.clearRect(0, 0, canvas.width, canvas.height)
+      
+    graphics.drawImage(
+		bgTexture,
+		bgX, bgY, bgWidth, bgHeight);
+    
+	graphics.drawImage(
+		marioTexture,
+		marioPositionX, marioPositionY, marioWidth, marioHeight);
+
+	graphics.drawImage(
+		goombaTexture,
+		goombaPositionX, goombaPositionY, goombaWidth, goombaHeight);
+
+  graphics.drawImage(
+	  miniMario,
+	  miniMarioX, miniMarioY, miniMarioWidth, miniMarioHeight);
+
+  graphics.drawImage(
+	  miniMario2,
+	  miniMario2X, miniMario2Y, miniMario2Width, miniMario2Height);
+
+  graphics.drawImage(
+	  miniMario3,
+	  miniMario3X, miniMario3Y, miniMario3Width, miniMario3Height);
+
+  graphics.drawImage(
+	  miniMario4,
+	  miniMario4X, miniMario4Y, miniMario4Width, miniMario4Height);
+  }
+
+  if (marioLives === 3) {
+    	graphics.clearRect(0, 0, canvas.width, canvas.height)
+      
+    graphics.drawImage(
+		bgTexture,
+		bgX, bgY, bgWidth, bgHeight);
+    
+	graphics.drawImage(
+		marioTexture,
+		marioPositionX, marioPositionY, marioWidth, marioHeight);
+
+	graphics.drawImage(
+		goombaTexture,
+		goombaPositionX, goombaPositionY, goombaWidth, goombaHeight);
+
+  graphics.drawImage(
+	  miniMario,
+	  miniMarioX, miniMarioY, miniMarioWidth, miniMarioHeight);
+
+  graphics.drawImage(
+	  miniMario2,
+	  miniMario2X, miniMario2Y, miniMario2Width, miniMario2Height);
+
+  graphics.drawImage(
+	  miniMario3,
+	  miniMario3X, miniMario3Y, miniMario3Width, miniMario3Height);
+  }
+
+  if (marioLives === 2) {
+    	graphics.clearRect(0, 0, canvas.width, canvas.height)
+      
+    graphics.drawImage(
+		bgTexture,
+		bgX, bgY, bgWidth, bgHeight);
+    
+	graphics.drawImage(
+		marioTexture,
+		marioPositionX, marioPositionY, marioWidth, marioHeight);
+
+	graphics.drawImage(
+		goombaTexture,
+		goombaPositionX, goombaPositionY, goombaWidth, goombaHeight);
+
+  graphics.drawImage(
+	  miniMario,
+	  miniMarioX, miniMarioY, miniMarioWidth, miniMarioHeight);
+
+  graphics.drawImage(
+	  miniMario2,
+	  miniMario2X, miniMario2Y, miniMario2Width, miniMario2Height);
+  }
+
+    if (marioLives === 1) {
+    	graphics.clearRect(0, 0, canvas.width, canvas.height)
+      
+    graphics.drawImage(
+		bgTexture,
+		bgX, bgY, bgWidth, bgHeight);
+    
+	graphics.drawImage(
+		marioTexture,
+		marioPositionX, marioPositionY, marioWidth, marioHeight);
+
+	graphics.drawImage(
+		goombaTexture,
+		goombaPositionX, goombaPositionY, goombaWidth, goombaHeight);
+
+  graphics.drawImage(
+	  miniMario,
+	  miniMarioX, miniMarioY, miniMarioWidth, miniMarioHeight);
+  }
+
+  if (marioLives === 0) {
+    	graphics.clearRect(0, 0, canvas.width, canvas.height)
+      
+    graphics.drawImage(
+		bg2Texture,
+		bg2X, bg2Y, bg2Width, bg2Height);
+  }
+
+	if( marioPositionX > canvas.width) {
+		marioPositionX = 0;
+  for (var i=marioLives; i<=marioLives; i-=1) {
+  if(i===5) {
+   marioLives -= 1;
+   break;
+  } else if(i===4) {
+   marioLives -= 1;
+   break;
+   
+ } else if(i===3) {
+   marioLives -= 1;
+   break;
+   
+ } else if(i===2) {
+   marioLives -= 1;
+   break;
+   
+ } else if(i===1) {
+  marioLives -= 1;
+   break;
+   
+ }else if(i===0) {
+   break;
+ } 
+ }
+ }
+ 
+
+	marioPositionY += gravity;
+
+	if( marioPositionY > canvas.height - marioHeight) {
+		marioPositionY = canvas.height - marioHeight;
+		jumpForce = maxJumpForce;
+	}
+
+
+	if (marioPositionX > goombaPositionX + goombaWidth){
+		goombaPositionX += goombaSpeed;
+	}
+
+	if(marioPositionX + marioWidth < goombaPositionX){
+		goombaPositionX -= goombaSpeed;
+	}
+
+	requestAnimationFrame(update);
+
 }
 
-
-// DO NOT EDIT BELOW THIS LINE
-//--------------------------------------------------------------------
 
 var keys = [];
 var LEFT_KEY = 37;
@@ -97,12 +298,11 @@ var UP_KEY = 38;
 var RIGHT_KEY = 39;
 var DOWN_KEY = 40;
 
-// check key down events
+
 window.addEventListener('keydown', function (event) {
 	keys[event.keyCode] = true;
 }, true);
 
-// check key release events
 window.addEventListener('keyup', function (event) {
 	keys[event.keyCode] = false;
 }, true);
@@ -112,4 +312,3 @@ function isKeyDown(key) {
 }
 
 update();
-//--------------------------------------------------------------------
