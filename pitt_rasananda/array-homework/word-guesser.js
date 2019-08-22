37e12eace5640c19024e4e36245ac7a1ab373782
YR <yui.rasananda@gmail.com>
2019-08-22 17:39:52 +1000

    fix issue where reward do not add up and where code won't exit

--- pitt_rasananda/array-homework/word-guesser.js
+++ pitt_rasananda/array-homework/word-guesser.js
@@ -1,43 +1,47 @@
 let secretWord = ['P','E','P','P','E','R'];
 let guessedLetters = ['_','_','_','_','_','_'];
 let reward = 0;
-let hangmanCount = 5;
 let letterCount = 0;
+let hangmanCount = 6;
 let usedCharacter = [];
 
 function guessLetter(letter) {
-  checkUsedCharacter(letter);
-  if (checkUsedCharacter() === true) {
-    
+  if (checkUsedCharacter(letter)) {
+    return;
   }
-    for(i=0; i<secretWord.length;i++) {
-        if (letter === secretWord[i]) {
-            guessedLetters.splice(i,1,letter);
-            letterCount ++;
-        } 
-    }
-    // if letter isn't in the secret word, add hangman count
-    if (letterCount === 0) {
-      console.log(`Sorry, this word doesn't contain letter ${letter}.`);
-      console.log(`You have ${hangmanCount} more tries.`);
-    // if letter is in secret word, congratulate and add reward.
+  
+  for(i=0; i<secretWord.length;i++) {
+    if (letter === secretWord[i]) {
+      guessedLetters.splice(i,1,letter);
+      letterCount ++;
+    } 
+  }
+
+// if letter isn't in the secret word, add hangman count
+// if letter is in secret word, congratulate and add reward.
+  if (letterCount === 0) {
+    console.log(`Sorry, this word doesn't contain letter ${letter}.`);
+    console.log(`You have ${hangmanCount-1} more tries.`);
+    hangmanCount --;
     } else if (letterCount >= 1) {
       console.log(`Congratulation! You've found the new letter!!!`)
       console.log(guessedLetters);
-      reward = Math.floor(Math.random() * 100 * letterCount); 
+      reward += Math.floor(Math.random() * 100 * letterCount); 
       console.log(`You've earn $ ${reward} for guessing correctly.\n`)
     }
-    usedCharacter.push(letter);
-    reward += reward;
-    checkLosing();
-    checkWinning();
+    
+  usedCharacter.push(letter);
+  checkLosing();
+  checkWinning();
+  letterCount = 0;
 }
+
 
 function checkUsedCharacter(letter){
   for(i=0; i<usedCharacter.length; i++) {
     if (letter === usedCharacter[i]) {
-      console.log(`You've already gussed ${letter}`);
-      return (true);
+      console.log(`You've already guessed ${letter}\n`);
+      return true;
     }
   } 
 }
@@ -48,7 +52,6 @@ function checkWinning() {
     console.log(`Your reward = $ ${reward}`);
   } else if (hangmanCount != 0) {
     console.log('Keep guessing!\n');
-    hangmanCount --; 
   }
 }
 
@@ -68,7 +71,13 @@ function checkLosing() {
 }
 
 
-guessLetter('P');
-guessLetter('E');
-guessLetter('P');
+
+
+
+
+
+
+
+
+
 
