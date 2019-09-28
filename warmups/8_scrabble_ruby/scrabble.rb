e88891e05db92e9586f1c23aae4f66b00f075d04
Yianni Moustakas <yianni.m13@gmail.com>
2019-09-28 10:15:23 +1000

    scrabble ruby solution

---
+++ warmups/8_scrabble_ruby/scrabble.rb
@@ -0,0 +1,28 @@
+@map = {
+	"A"=>1, "E"=>1, "I"=>1, "O"=>1, "U"=>1, "L"=>1, "N"=>1, "R"=>1, "S"=>1, "T"=>1,
+	"D"=>2, "G"=>2,
+	"B"=>3, "C"=>3, "M"=>3, "P"=>3,
+	"F"=>4, "H"=>4, "V"=>4, "W"=>4, "Y"=>4,
+	"K"=>5,
+	"J"=>8, "X"=>8,
+	"Q"=>10, "Z"=>10
+}
+
+def	calc_score word
+
+	score = 0
+
+	word.upcase.each_char do |letter|
+		score += @map[letter]
+	end
+
+	score
+end
+
+puts "Please enter a word"
+
+input = "cabbage"
+output = calc_score input
+
+puts "Your word: #{input}"
+puts "Your word score: #{output}"
