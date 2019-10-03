eea9261b55a5abacdf129755f768808845ab844a
Yianni Moustakas <yianni.m13@gmail.com>
2019-10-03 18:27:12 +1000

    nucleotide solution

---
+++ warmups/9_nucleotide_count/nuc_count.rb
@@ -0,0 +1,21 @@
+@bases = {
+	"A" => 0,
+	"C" => 0,
+	"G" => 0,
+	"T" => 0,
+	"Unknown" => 0
+}
+
+def count input 
+
+  input.each_char do |letter|
+    if @bases.key? letter
+      @bases[letter] += 1
+    else 
+      @bases["Unknown"] += 1
+		end
+  end
+  puts @bases
+end
+
+count "ACGTCGATCAGTCAGGCAGCATTATCATCAGCAVGCATAC"
