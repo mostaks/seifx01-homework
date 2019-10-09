9f5693e6be9ed2d21e4e5e7379ef50e5ccc8f0b0
53417 <seanimal@Live.com.au>
2019-10-10 01:48:45 +1100

    update 2019-10-08

---
+++ sean_cooper/2019-10-08/movies+books/init.sql
@@ -0,0 +1,15 @@
+CREATE TABLE books (
+	id INTEGER PRIMARY KEY,
+	book_name TEXT,
+	book_author TEXT,
+	book_description TEXT,
+	book_rating TEXT,
+	book_img TEXT
+);
+
+INSERT INTO books (id, book_name, book_author, book_description, book_rating, book_img) VALUES (1, "Selby", "Selby", "dog diary", "7/10", "https://www.placecage.com/300/200");
+INSERT INTO books (id, book_name, book_author, book_description, book_rating, book_img) VALUES (2, "Harry Potter", "JKR", "youre a wizard harry", "8/10", "https://www.placecage.com/300/200");
+INSERT INTO books (id, book_name, book_author, book_description, book_rating, book_img) VALUES (3, "Dictionary", "The word man", "snore", "10/10", "https://www.placecage.com/300/200");
+INSERT INTO books (id, book_name, book_author, book_description, book_rating, book_img) VALUES (4, "Percy Jackson", "not JKR", "harry potter, but worse", "5/10", "https://www.placecage.com/300/200");
+INSERT INTO books (id, book_name, book_author, book_description, book_rating, book_img) VALUES (5, "IT", "Stephen King", "i'm lovin it", "5/10", "https://www.placecage.com/300/200");
+INSERT INTO books (id, book_name, book_author, book_description, book_rating, book_img) VALUES (6, "1984", "Pooh bear", "Hong Kong predicted", "8/10", "https://www.placecage.com/300/200")
