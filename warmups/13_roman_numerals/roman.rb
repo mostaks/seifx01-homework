3d9f83bb4a713738d32bda3d07b4e8390a57a6da
Yianni Moustakas <yianni.m13@gmail.com>
2019-10-19 10:30:00 +1100

    refactor roman map outside of the initialize method

--- warmups/13_roman_numerals/roman.rb
+++ warmups/13_roman_numerals/roman.rb
@@ -1,27 +1,27 @@
 
 class Roman
-def initialize number
-  @number = number.to_i
-    @roman_map = {
-      1000 => "M",
-      900 => "CM",
-      500 => "D",
-      400 => "CD",
-      100 => "C",
-      90 => "XC",
-      50 => "L",
-      40 => "XL",
-      10 => "X",
-      9 => "IX",
-      5 => "V",
-      4 => "IV",
-      1 => "I"
-    }
-end
+  @@roman_map = {
+    1000 => "M",
+    900 => "CM",
+    500 => "D",
+    400 => "CD",
+    100 => "C",
+    90 => "XC",
+    50 => "L",
+    40 => "XL",
+    10 => "X",
+    9 => "IX",
+    5 => "V",
+    4 => "IV",
+    1 => "I"
+  }
+  def initialize number
+    @number = number.to_i
+  end
 
   def to_roman
     result = ""
-    @roman_map.each do |key, value|
+    @@roman_map.each do |key, value|
       while @number >= key
         result += value
         puts "number : #{@number} >= #{key}"
