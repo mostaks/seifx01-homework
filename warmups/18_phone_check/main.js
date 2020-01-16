e96075b56dc5e1e7a858770d7154dec5e7ab8578
Yianni Moustakas <yianni.m13@gmail.com>
2020-01-16 18:43:12 +1100

    phone solution

---
+++ warmups/18_phone_check/main.js
@@ -0,0 +1,22 @@
+const cleanNumber = num => {
+	let cleanNum = num.replace(/[^0-9]+/g, "");
+	return (cleanNum.length === 11 && cleanNum[0] === '1') ? cleanNum.substr(1) : cleanNum;
+}
+
+const isValid = num => {
+	let cleaned = cleanNumber(num);
+	return cleaned.length !== 10 ? '0000000000' : cleaned;
+}
+
+const getFormattedNumber = num => {
+	let validNum = isValid(num);
+	console.log('validNum', validNum);
+	const areaCode = validNum.substr(0, 3);
+	const exchangeCode = validNum.substr(3, 3)
+	const remaining = validNum.substr(6);
+	return `(${areaCode}) ${exchangeCode}-${remaining}`;
+}
+
+const num1 = "132345s673w#8mm";
+
+console.log(getFormattedNumber(num1));
